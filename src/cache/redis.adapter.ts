c5e776b19414e9fca7d2453fc79b3bc4a9a58e78
Hannah <ghdcoalss33@gmail.com>
2023-05-12 13:57:53 +0900

    웨않됨?

--- src/cache/redis.adapter.ts
+++ src/cache/redis.adapter.ts
@@ -7,14 +7,14 @@ import { ServerOptions } from 'socket.io';
 // 작은 프로젝트인데 클러스터를 사용해야하나? 라는 생각에 미사용이다...
 // 사용하려면 main.ts에 아래 코드를 추가해주어야한다
 // app.useWebSocketAdapter(new RedisIoAdapter(app));
-export class RedisIoAdapter extends IoAdapter {
-  createIOServer(port: number, options?: ServerOptions): any {
-    const server = super.createIOServer(port, options);
-    const url = `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
-    const pubClient = createClient({ url: url });
-    const subClient = pubClient.duplicate();
-
-    server.adapter(createAdapter(pubClient, subClient));
-    return server;
-  }
-}
+// export class RedisIoAdapter extends IoAdapter {
+//   createIOServer(port: number, options?: ServerOptions): any {
+//     const server = super.createIOServer(port, options);
+//     const url = `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+//     const pubClient = createClient({ url: url });
+//     const subClient = pubClient.duplicate();
+//
+//     server.adapter(createAdapter(pubClient, subClient));
+//     return server;
+//   }
+// }
